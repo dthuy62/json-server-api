ef4445f496b881365a04c98f69c635f4496817ff
dthuy0602 <dthuy.18it2@sict.udn.vn>
2022-01-19 14:59:46 +0700

    add auth api

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ const queryString = require('query-string');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser')
+const fs = require('fs');
+
+const userdb = JSON.parse(fs.readFileSync('./users.json', 'UTF-8'))
+
+
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
@@ -15,6 +22,11 @@ server.get('/echo', (req, res) => {
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser);
+
+server.use(bodyParser.urlencoded({extended: true}))
+server.use(bodyParser.json())
+server.use(jsonServer.defaults());
+
 server.use((req, res, next) => {
   if (req.method === 'POST') {
     req.body.createdAt = Date.now();
@@ -26,6 +38,108 @@ server.use((req, res, next) => {
   // Continue to JSON Server router
   next();
 });
+
+const SECRET_KEY = 'namthao';
+const expiresIn = '1h';
+
+// Create a token from a payload
+function createToken(payload) {
+  return jwt.sign(payload, SECRET_KEY, { expiresIn });
+}
+// Verify the token
+function verifyToken(token) {
+  return jwt.verify(token, SECRET_KEY, (err, decode) => decode !== undefined ? decode : err )
+}
+// Check if the user exists in database
+function isAuthenticated({email, password}) {
+  return userdb.users.findIndex(user => user.email === email && user.password === password) !== -1
+}
+// Register new user
+server.post('/api/auth/register', (req, res) => {
+  console.log("register endpoint called; request body:");
+  console.log(req.body);
+  const { email, password } = req.body;
+
+  if(isAuthenticated({email, password}) === true) {
+    const status = 401;
+    const message = 'Email or password already exists';
+    res.status(status).json({ status, message });
+    return;
+  }
+
+  fs.readFile("./users.json", (err, data) => {
+    if(err) {
+      const status = 401;
+      const message = err;
+      res.status(status).json({ status, message })
+      return;
+    };
+
+  //  Get current user data
+    var data = JSON.parse(data.toString());
+  //  Get id of the last user
+    var last_item_id = data.users[data.users.length-1].id;
+  //  Add new user
+    data.users.push({id: last_item_id + 1, email: email, password: password}); // add some data
+    var writeData = fs.writeFile("./users.json", JSON.stringify(data), (err, result) => {
+      if (err) {
+        const status = 401
+        const message = err
+        res.status(status).json({status, message})
+        return
+      }
+    })
+  })
+  const access_token = createToken({email, password})
+  console.log("Access Token:" + access_token);
+  res.status(200).json({access_token})
+
+});
+// Create token for new user
+
+
+// Login to one of the users from ./users.json
+server.post('/api/auth/login', (req, res) => {
+  console.log("login endpoint called; request body:");
+  console.log(req.body);
+  const {email, password} = req.body;
+  if (isAuthenticated({email, password}) === false) {
+    const status = 401
+    const message = 'Incorrect email or password'
+    res.status(status).json({status, message})
+    return
+  }
+  const access_token = createToken({email, password})
+  console.log("Access Token:" + access_token);
+  res.status(200).json({access_token})
+})
+
+server.use(/^(?!\/auth).*$/,  (req, res, next) => {
+  if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
+    const status = 401
+    const message = 'Error in authorization format'
+    res.status(status).json({status, message})
+    return
+  }
+  try {
+    let verifyTokenResult;
+    verifyTokenResult = verifyToken(req.headers.authorization.split(' ')[1]);
+
+    if (verifyTokenResult instanceof Error) {
+      const status = 401
+      const message = 'Access token not provided'
+      res.status(status).json({status, message})
+      return
+    }
+    next()
+  } catch (err) {
+    const status = 401
+    const message = 'Error access_token is revoked'
+    res.status(status).json({status, message})
+  }
+})
+
+
 
 // Custom output for LIST with pagination
 router.render = (req, res) => {
